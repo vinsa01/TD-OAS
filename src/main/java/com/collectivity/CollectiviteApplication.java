8dfaa8a8f7c7b5f230621cdcae6986b49e7924d8
vinsa01 <nata.lazasoa@gmail.com>
2026-04-22 08:53:22 +0300

    Update CollectiviteApplication.java

--- src/main/java/com/collectivity/CollectiviteApplication.java
+++ src/main/java/com/collectivity/CollectiviteApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CollectiviteApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(CollectiviteApplication.class, args);
     }
